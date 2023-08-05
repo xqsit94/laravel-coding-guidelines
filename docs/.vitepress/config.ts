4c35f2a8b79e02f44346bc75f37e5bdb679ec446
Manikandan B <xqsit94@gmail.com>
2023-08-05 17:33:27 +0530

    Github pages deploy configurations

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Laravel Coding Guidelines",
   description: "Laravel Coding Guidelines",
+  base: '/laravel-coding-guidelines/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     siteTitle: 'Laravel Coding Guidelines',
